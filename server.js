3ba6d481b1d096e9b8ffdd0992f746fe77470c1c
TrueGamerTalha <69008390+TrueGamerTalha@users.noreply.github.com>
2021-04-15 16:19:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -177,3 +177,36 @@ $setUserVar[afkwhy;;$authorID]
 Hello <@$authorID>, I have removed your AFK
 $onlyIf[$getUserVar[afk;$authorID]>=1;]`
 })
+
+bot.command({
+name: "ticketpanel", 
+code: `
+$reactionCollector[$get[message];everyone;24d;ðŸ“:registered:;createticket;yes]
+$let[message;$sendMessage[{description:$getServerVar[panelmessage]};yes]]
+$onlyPerms[admin;Only admins can use this]
+$onlyIf[$getServerVar[ticketcategory]!=unset;No Ticketcategory has been set yet. Please contact some server staff to change this by using \`$getServerVar[prefix]set-ticketcategory (CategoryID)\`.]
+$onlyIf[$getServerVar[premium]==true;This is a Beta Feature. You need premium to use beta features]
+`
+})
+
+bot.awaitedCommand({
+name: "createticket",
+code: `
+$newTicket[ticket-$username;|| <@$authorID> ||
+{description:<@$authorID> - $getServerVar[ticketmessage]
+__Subject__
+\`\`\`Opened with Panel\`\`\`} {title:$username's ticket} {footer: Use \`$getServerVar[prefix]close-ticket\` to close the ticket} {color:00ff00};$getServerVar[ticketcategory];no;:x: Fatal error! Try setting the Ticketcategory and cooldown again. If thats not working contact some bot staff on the Support Server!]
+$setUserVar[subject;$message]
+$cooldown[$getServerVar[ticketcooldown];Sorry, you can't create another ticket at the moment. You have to wait %time% until you can use this command again!]
+$onlyIf[$getServerVar[ticketcategory]!=unset;No Ticketcategory has been set yet. Please contact some server staff to change this by using \`$getServerVar[prefix]set-ticketcategory (CategoryID)\`.]`})
+
+bot.command({
+name: "set-panelmessage", 
+code: `
+You have set the Panel message to: $description[$message]
+$setServerVar[panelmessage;$message]
+$argsCheck[<=1;Please write at least more that 1 word]
+$onlyPerms[admin;Only admins can use this]
+$onlyIf[$getServerVar[premium]==true;This is a Beta Feature. You need premium to use beta features]
+`
+})
