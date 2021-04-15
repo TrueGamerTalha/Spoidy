baac17b9088966a86224e5b37941371ec80ed241
TrueGamerTalha <69008390+TrueGamerTalha@users.noreply.github.com>
2021-04-15 16:26:17 +0600

    Create closeticket.js

---
+++ commands/ticket/closeticket.js
@@ -0,0 +1,7 @@
+module.exports = {
+name: "close-ticket",
+code: `$closeTicket[Error at closing Ticket, maybe you are not in a ticket channel :/]
+$onlyPerms[managemessages;You dont have the permissions "managemessages"]
+
+`
+}
